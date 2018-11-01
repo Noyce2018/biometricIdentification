41462c968441751191f96ce6f1f682235b703a2a
TianYan_AppTeam_TBL <tianyan_appteam_tbl@tbl.local>
2018-11-01 15:07:17 +0800

    去掉不必要的注释

--- src/ios/biometricIdentification.h
+++ src/ios/biometricIdentification.h
@@ -1,7 +1,6 @@
 #import <Cordova/CDV.h>
 
 @interface biometricIdentification : CDVPlugin {
-    // Member variables go here.
     CDVPluginResult* pluginResult;
 }
 - (void)BiometricIdentification:(CDVInvokedUrlCommand*)command;
